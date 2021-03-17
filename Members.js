2a4cf68cb3fe6644e9d76f0db2f9cfe6c103b5cf
Hrishikesh-coder <hrishipotter123@gmail.com>
2021-03-17 17:30:19 +0530

    added more code

---
+++ Members.js
@@ -0,0 +1,23 @@
+const members = [
+	{
+		id:1,
+		name:'abc',
+		email:'abc@xyz.com',
+		status:'active'
+	},
+	{
+		id:2,
+		name:'def',
+		email:'def@xyz.com',
+		status:'inactive'
+	},
+	{
+		id:3,
+		name:'ghi',
+		email:'ghi@xyz.com',
+		status:'active'
+	}
+
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -4,12 +4,26 @@ const path = require('path');
 
 const app = express();
 
+const members = require('./Members');
+
+const logger = require('./middleware/logger');
+
+app.use(logger);
+
+//gets all members
+
+app.get('/api/members', (req,res) => {
+	res.json(members);
+});
+
 // app.get('/', (req,res)=>{
 // 	//res.send('<h1>Hello World!!</h1>');
 // 	res.sendFile(path.join(__dirname,'public','index.html'));
 // })
 
-//Set static 
+//Set static folder
+
+app.use(express.static(path.join(__dirname,'public')));
 
 const PORT = process.env.PORT || 5000;
 
